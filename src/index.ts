0f5ab089e0ef015d1c2d045e75e057baace84f50
spouki <iyadahmedhamoudi@gmail.com>
2024-07-12 17:10:13 +0100

    creating the task object

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+import { v4 as uuidV4} from "uuid"
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit" , e =>{
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null)return
+  
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed : false,
+    createdAt : new Date()
+  }
+
+  input.value
+})
